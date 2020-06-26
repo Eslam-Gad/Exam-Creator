6d4ea6329c10ae427df76f59bc12aa787da0b93a
EslamGad <eslamhendy990@gmail.com>
2020-06-26 23:53:51 +0200

    last one

--- app/src/main/java/com/EX/examcreator/OnBoardingSuppliers.java
+++ app/src/main/java/com/EX/examcreator/OnBoardingSuppliers.java
@@ -10,8 +10,7 @@ public class OnBoardingSuppliers {
 
     public static List<OnBoardingModel> getOnBoardingObjects() {
         ArrayList<OnBoardingModel> object = new ArrayList<OnBoardingModel>();
-        object.add(new OnBoardingModel(R.drawable.logo1
-                ,"Exam Creator","You Append Questions, We Create Exam "));
+        object.add(new OnBoardingModel(R.drawable.logo,"Exam Creator","You Append Questions, We Create Exam "));
         object.add(new OnBoardingModel(R.drawable.st_wifi,"Network","Must Connect With Internet to Use App"));
         object.add(new OnBoardingModel(R.drawable.st_fav,"Favourite","Favourite any Questions to Browse Offline"));
         object.add(new OnBoardingModel(R.drawable.st_swipe,"Swipe","Swipe list Items to Delete and Update"));
